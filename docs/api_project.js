940af275bc378800db0061ebfbdd82f0597f7ca9
Timo Volk <timo.volk@gmail.com>
2018-06-02 11:34:17 +0200

    docs folder

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "enviroomate-server",
+  "version": "0.0.1",
+  "description": "Awesome project developed with TypeORM.",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-06-02T09:33:24.717Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
