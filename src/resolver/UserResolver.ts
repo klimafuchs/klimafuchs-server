3179ef0ccded1d26fe09bb720f77e38f0a8a5993
vovin <timo.volk@gmail.com>
2018-11-19 14:41:17 +0100

    GQL themenwoche -> Challenge fix

--- src/resolver/UserResolver.ts
+++ src/resolver/UserResolver.ts
@@ -45,7 +45,7 @@ export class UserResolver {
         user.screenName= userInput.screenName ||user.screenName;
         user.userName = userInput.userName || user.userName;
         user.isBanned = (userInput.isBanned === undefined) ? user.isBanned : userInput.isBanned;
-        user.emailConfirmed = (userInput.emailConfirmed === undefined) ? user.emailConfirmed ? userInput.emailConfirmed;
+        user.emailConfirmed = (userInput.emailConfirmed === undefined) ? user.emailConfirmed : userInput.emailConfirmed;
         // TODO reset Avatars
         return this.userRepository.save(user)
     }
