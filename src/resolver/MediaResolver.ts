5bcc47e4dc8c9b6fcd01ac6512472e16784ea325
vovin <timo.volk@gmail.com>
2018-11-30 13:42:12 +0100

    image upload dir set

--- src/resolver/MediaResolver.ts
+++ src/resolver/MediaResolver.ts
@@ -10,7 +10,7 @@ import {User} from "../entity/user/User";
 
 @Resolver(Media)
 export class MediaResolver {
-    private static uploadDir: String = './img';
+    private static uploadDir: string = './img';
 
     constructor(
         @InjectRepository(Media) private readonly mediaRepository: Repository<Media>,
