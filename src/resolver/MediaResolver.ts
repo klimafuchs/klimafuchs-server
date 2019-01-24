e9c4349a15dd63cd8cea736b8ba29266d62dfb81
vovin <timo.volk@gmail.com>
2019-01-24 14:48:19 +0100

    logging uploads

--- src/resolver/MediaResolver.ts
+++ src/resolver/MediaResolver.ts
@@ -43,7 +43,7 @@ export class MediaResolver {
     @Mutation(returns => Media)
     async upload(@Arg('file', type => GraphQLUpload) file: Upload, @Ctx() {user}: Context): Promise<Media> {
         const {stream, filename, mimetype, encoding, width, height} = await file;
-        console.log(`Receiving file filename: ${filename}, mimetype: ${mimetype}, encoding: ${encoding}, height: ${height}, width: ${width}`∆)
+        console.log(`Receiving file filename: ${filename}, mimetype: ${mimetype}, encoding: ${encoding}, height: ${height}, width: ${width}`)
         ;
         const {path, newName} = await MediaResolver.storeFile(stream, filename);
         const media = this.mediaRepository.create({
